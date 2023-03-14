2ce43b0bbb986122d29c9302872739a9dc065b5d
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-03-14 16:30:39 -0600

    add getAppointmentsForDay selector and its unit test in selectors.test.js

---
+++ src/helpers/selectors.js
@@ -0,0 +1,9 @@
+export function getAppointmentsForDay(state, day) {
+  let appoiArray = [];
+  let appointments = [];
+  state.days.forEach(element => element.name === day ? appoiArray = element.appointments : []);
+    //appoiArray = the appointments array of that day = [1,2,3]
+  appoiArray.forEach(id => state.appointments[id] ? appointments.push(state.appointments[id]) : true);
+  return appointments;
+};
+

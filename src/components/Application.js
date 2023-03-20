eba61d7e13a592d88de034cd103cf8436789d4d2
Oanh Nguyen <oanh@Gradient-MacBook-Air.local>
2023-03-20 08:39:21 -0600

    add useApplicationData custom hook to manage data instead of application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,85 +1,31 @@
-import React, {useState, Fragment, useEffect} from "react";
-import axios from 'axios';
+import React from "react";
 
 import "components/Application.scss";
+
 import {getAppointmentsForDay, getInterview, getInterviewersForDay} from '../helpers/selectors';
+import {useApplicationData} from '../hooks/useVisualMode';
 
 import DayList from './DayList';
 import Appointment from 'components/Appointment/index';
 
-export default function Application() {
-  const [state, setState] = useState({
-    day: 'Monday',
-    days: [],
-    appointments: {},
-    interviewers: {}
-  });
-  const setDay = day => setState({ ...state, day });
+//This component will only be responsible for passing data to other components.
+//The useApplicationData custom Hook owns the data management.
+export default function Application() { 
+  const {
+    state,
+    setDay,
+    bookInterview,
+    cancelInterview
+  } = useApplicationData();
 
-  useEffect(() => {
-    Promise.all([
-      axios.get('/api/days'),
-      axios.get('/api/appointments'),
-      axios.get('/api/interviewers')
-    ]).then((res) => {
-      setState(prev => ({...prev, days: res[0].data, appointments: res[1].data, interviewers: res[2].data }));
-      //update data for days and appointments in our state at the same time.
-    })
-  }, []);
-  //to check if the state obj has changed: open the "React Components" tab in Dev Tools and select the Application component. Confirm that the state is set after the days and appointments requests are complete
-  //This is one approach to solving our data dependency problem. No dependency or empty array needed
-
-  function bookInterview(id, interview) { //appointment id and interview from onSave func in Form component
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview } //interview key is updated with interview obj from a newly added appointment
-    };
-    
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    };
-
-    return axios.put(`api/appointments/${id}`, {interview}) //this is for the back-end, go to one appointment and update its interview {} with the {interview} that we sent
-      .then(() => {
-        setState( prev => ({
-          ...prev,
-          appointments //it's the newly updated appointments obj
-        })); //setState is only for the client side. w/t the back-end update, when we refresh the page, the newly added slot will disappear.
-      })
-    //note: here, the state is changed locally at Application-level
-  };
-
-  function cancelInterview(id) {
-    const appointment = {
-      ...state.appointments[id],
-      interview: null
-    }
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment
-    }
-
-    return axios.delete(`http://localhost:8001/api/appointments/${id}`) //delete the whole appointment in the appointments obj
-      .then(() => {
-        setState({ //setState = refresh button, the whole browser will re-render everything again.
-          ...state, 
-          appointments
-        })
-      })
-  }
-
-  const dailyAppointments = getAppointmentsForDay(state, state.day); //[{appointment},{appointment},..]
   const dailyinterviewers = getInterviewersForDay(state, state.day); //[{interviewer},{interviewer},..]
-  
+  const dailyAppointments = getAppointmentsForDay(state, state.day); //[{appointment},{appointment},..]
   const EachAppointment = dailyAppointments.map(appointment => {
-    const interview = getInterview(state, appointment.interview); //an object of student and interviewers of an appointment
-
     return (
       <Appointment
         key={appointment.id}
         {...appointment}
-        interview={interview}
+        interview={getInterview(state, appointment.interview)} //an object of student and interviewers of an appointment
         interviewers={dailyinterviewers}
         bookInterview={bookInterview}
         cancelInterview={cancelInterview}
